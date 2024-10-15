6e238fe2997c6bae756b9f7ae028abe9fe7dc2de
yaarySh <yaary1526@gmail.com>
2024-10-15 18:21:31 +0300

    change carts and global urls

--- carts/urls.py
+++ carts/urls.py
@@ -3,12 +3,10 @@ from carts import views
 
 urlpatterns = [
     path(
-        "cart/", views.get_cart, name="get_cart"
+        "", views.get_cart, name="get_cart"
     ),  # To retrieve the cart for the authenticated user
+    path("add/", views.add_to_cart, name="add_to_cart"),  # To add a product to the cart
     path(
-        "cart/add/", views.add_to_cart, name="add_to_cart"
-    ),  # To add a product to the cart
-    path(
-        "cart/item/<int:item_id>/", views.update_cart_item, name="update_cart_item"
+        "<int:item_id>/", views.update_cart_item, name="update_cart_item"
     ),  # To update or delete a specific cart item
 ]
--- myshop/urls.py
+++ myshop/urls.py
@@ -9,7 +9,7 @@ urlpatterns = [
     path("admin/", admin.site.urls),
     path("products/", include("products.urls")),
     path("users/", include("users.urls")),
-    path("carts/", include("carts.urls")),
+    path("cart/", include("carts.urls")),
 ]
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
