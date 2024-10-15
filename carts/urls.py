024d225e10f848f60cbdfceea9b65b93bcb25888
yaarySh <yaary1526@gmail.com>
2024-10-15 18:34:58 +0300

    undoing last commit

--- carts/urls.py
+++ carts/urls.py
@@ -3,10 +3,12 @@ from carts import views
 
 urlpatterns = [
     path(
-        "", views.get_cart, name="get_cart"
+        "cart/", views.get_cart, name="get_cart"
     ),  # To retrieve the cart for the authenticated user
-    path("add/", views.add_to_cart, name="add_to_cart"),  # To add a product to the cart
     path(
-        "<int:item_id>/", views.update_cart_item, name="update_cart_item"
+        "cart/add/", views.add_to_cart, name="add_to_cart"
+    ),  # To add a product to the cart
+    path(
+        "cart/item/<int:item_id>/", views.update_cart_item, name="update_cart_item"
     ),  # To update or delete a specific cart item
 ]
